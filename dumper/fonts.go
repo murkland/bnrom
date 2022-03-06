af6a7114617e7011b9a7199cfcac04ff5e7e97b8
bigfarts <bigfarts@punymail.com>
2022-03-06 15:17:08 -0800

    more fonts

--- dumper/fonts.go
+++ dumper/fonts.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"image"
@@ -26,44 +27,120 @@ func dumpFonts(r io.ReadSeeker, outFn string) error {
 
 	os.Mkdir(outFn, 0o700)
 
-	if _, err := r.Seek(info.EnemyHPOffset, io.SeekStart); err != nil {
-		return fmt.Errorf("%w while seeking to enemy hp font pointer", err)
+	if _, err := r.Seek(info.TinyOffset, io.SeekStart); err != nil {
+		return fmt.Errorf("%w while seeking to tiny font pointer", err)
 	}
 
-	if err := dumpEnemyHPFont(r, outFn+"/enemyhp.bdf"); err != nil {
-		return fmt.Errorf("%w while dumping enemy hp font", err)
+	if err := dumpTinyFont(r, outFn+"/tiny.bdf"); err != nil {
+		return fmt.Errorf("%w while dumping tiny font", err)
+	}
+
+	if _, err := r.Seek(info.TallOffset, io.SeekStart); err != nil {
+		return fmt.Errorf("%w while seeking to tall font pointer", err)
+	}
+
+	if err := dumpTallFont(r, info.Charmap, outFn+"/tall.bdf"); err != nil {
+		return fmt.Errorf("%w while dumping tall font", err)
 	}
 
 	return nil
 }
 
-func dumpEnemyHPFont(r io.ReadSeeker, enemyHPOutFn string) error {
-	enemyHPGlyphs, err := fonts.ReadEnemyHPGlyphs(r)
-	if err != nil {
-		return fmt.Errorf("%w while reading enemy hp font", err)
+func dumpTinyFont(r io.ReadSeeker, outFn string) error {
+	const n = 10
+
+	var offsets []int64
+	for i := 0; i < n; i++ {
+		var offset uint32
+		if err := binary.Read(r, binary.LittleEndian, &offset); err != nil {
+			return fmt.Errorf("%w while reading offset to tiny font", err)
+		}
+		offsets = append(offsets, int64(offset&^0x08000000))
 	}
 
-	outF, err := os.Create(enemyHPOutFn)
+	glyphs := make([]*image.Paletted, len(offsets))
+
+	for i, offset := range offsets {
+		if _, err := r.Seek(offset, io.SeekStart); err != nil {
+			return fmt.Errorf("%w while seeking to tiny font pointer", err)
+		}
+
+		glyph, err := fonts.ReadGlyph(r, 5)
+		if err != nil {
+			return fmt.Errorf("%w while reading tiny font", err)
+		}
+
+		glyphs[i] = glyph
+	}
+
+	outF, err := os.Create(outFn)
 	if err != nil {
 		return err
 	}
 	defer outF.Close()
 
 	p := bdf.Properties{
-		XLFD:      "-murkland-enemyhp-medium-r-normal--16-160-75-75-c-80-iso10646-1",
+		XLFD:      "-murkland-tiny-medium-r-normal--16-160-75-75-c-80-iso10646-1",
 		Size:      16,
 		DPI:       image.Point{75, 75},
 		BBox:      image.Rect(0, 0, 8, 16),
 		Ascent:    12,
 		Descent:   2,
-		NumGlyphs: len(enemyHPGlyphs),
+		NumGlyphs: len(glyphs),
 	}
 	if err := bdf.WriteProperties(outF, p); err != nil {
 		return fmt.Errorf("%w while writing bdf properties", err)
 	}
 
-	for i, glyph := range enemyHPGlyphs {
+	for i, glyph := range glyphs {
 		if err := bdf.WriteGlyph(outF, p, rune(strconv.Itoa(i)[0]), glyph); err != nil {
+			return fmt.Errorf("%w while writing bdf properties", err)
+		}
+	}
+
+	if err := bdf.WriteTrailer(outF); err != nil {
+		return fmt.Errorf("%w while writing bdf trailer", err)
+	}
+
+	return nil
+}
+
+func dumpTallFont(r io.ReadSeeker, charmap []rune, outFn string) error {
+	var offset uint32
+	if err := binary.Read(r, binary.LittleEndian, &offset); err != nil {
+		return fmt.Errorf("%w while reading offset to tall font", err)
+	}
+
+	if _, err := r.Seek(int64(offset&^0x08000000), io.SeekStart); err != nil {
+		return fmt.Errorf("%w while seeking to tall font pointer", err)
+	}
+
+	outF, err := os.Create(outFn)
+	if err != nil {
+		return err
+	}
+	defer outF.Close()
+
+	p := bdf.Properties{
+		XLFD:      "-murkland-tall-medium-r-normal--16-160-75-75-c-80-iso10646-1",
+		Size:      16,
+		DPI:       image.Point{75, 75},
+		BBox:      image.Rect(0, 0, 8, 16),
+		Ascent:    12,
+		Descent:   2,
+		NumGlyphs: 448,
+	}
+	if err := bdf.WriteProperties(outF, p); err != nil {
+		return fmt.Errorf("%w while writing bdf properties", err)
+	}
+
+	for i := 0; i < p.NumGlyphs; i++ {
+		glyph, err := fonts.ReadGlyph(r, 1)
+		if err != nil {
+			return fmt.Errorf("%w while reading tall font glyph %d", err, i)
+		}
+
+		if err := bdf.WriteGlyph(outF, p, charmap[i], glyph); err != nil {
 			return fmt.Errorf("%w while writing bdf properties", err)
 		}
 	}
--- fonts/mod.go
+++ fonts/mod.go
@@ -1,15 +1,50 @@
 package fonts
 
+import (
+	"image"
+	"image/color"
+	"io"
+
+	"github.com/murkland/bnrom/sprites"
+)
+
 type ROMInfo struct {
-	EnemyHPOffset int64
+	TinyOffset int64
+	TallOffset int64
+	Charmap    []rune
 }
 
 func FindROMInfo(romID string) *ROMInfo {
 	switch romID {
 	case "BR6E", "BR6P", "BR5E", "BR5P":
-		return &ROMInfo{0x0001D854}
+		return &ROMInfo{0x0001D854, 0x0001C824, []rune(" 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ*abcdefghijklmnopqrstuvwxyz�����ウアイオエケコカクキセサソシステトツタチネノヌナニヒヘホハフミマメムモヤヨユロルリレラン熱斗ワヲギガゲゴグゾジゼズザデドヅダヂベビボバブピパペプポゥァィォェュヴッョャ-×=:%?+█�ー!&,゜.・;'\"~/()「」�_�����あいけくきこかせそすさしつとてたちねのなぬにへふほはひめむみもまゆよやるらりろれ�んをわ研げぐごがぎぜずじぞざでどづだぢべばびぼぶぽぷぴぺぱぅぁぃぉぇゅょっゃ容量全木�無現実◯✗緑道不止彩起父集院一二三四五六七八陽十百千万脳上下左右手来日目月獣各人入出山口光電気綾科次名前学校省祐室世界高朗枚野悪路闇大小中自分間系花問究門城王兄化葉行街屋水見終新桜先生長今了点井子言太属風会性持時勝赤代年火改計画職体波回外地員正造値合戦川秋原町晴用金郎作数方社攻撃力同武何発少教以白早暮面組後文字本階明才者向犬々ヶ連射舟戸切土炎伊夫鉄国男天老師堀杉士悟森霧麻剛垣")}
 	case "BR6J", "BR5J":
-		return &ROMInfo{0x0001DC78}
+		return &ROMInfo{0x0001DC78, 0x0001CC48, []rune(" 0123456789ウアイオエケコカクキセサソシステトツタチネノヌナニヒヘホハフミマメムモヤヨユロルリレラン熱斗ワヲギガゲゴグゾジゼズザデドヅダヂベビボバブピパペプポゥァィォェュヴッョャABCDEFGHIJKLMNOPQRSTUVWXYZ*-×=:%?+■�ー!��&、゜.・;’\"~/()「」����_�周えおうあいけくきこかせそすさしつとてたちねのなぬにへふほはひめむみもまゆよやるらりろれ�んをわ研げぐごがぎぜずじぞざでどづだぢべばびぼぶぽぷぴぺぱぅぁぃぉぇゅょっゃabcdefghijklmnopqrstuvwxyz容量全木�無現実◯✗緑道不止彩起父集院一二三四五六七八陽十百千万脳上下左右手来日目月獣各人入出山口光電気綾科次名前学校省祐室世界高朗枚野悪路闇大小中自分間系花問究門城王兄化葉行街屋水見終新桜先生長今了点井子言太属風会性持時勝赤代年火改計画職体波回外地員正造値合戦川秋原町晴用金郎作数方社攻撃力同武何発少教以白早暮面組後文字本階明才者向犬々ヶ連射舟戸切土炎伊夫鉄国男天老師堀杉士悟森霧麻剛垣")}
 	}
 	return nil
+}
+
+var fontPalette = color.Palette{
+	color.RGBA{0, 0, 0, 0},
+	color.RGBA{0, 0, 0, 255},
+}
+
+func ReadGlyph(r io.ReadSeeker, opaqueColor uint8) (*image.Paletted, error) {
+	glyph := image.NewPaletted(image.Rect(0, 0, 8, 16), fontPalette)
+	for o := 0; o < 2; o++ {
+		tile, err := sprites.ReadTile(r)
+		if err != nil {
+			return nil, err
+		}
+
+		for j := 0; j < 8; j++ {
+			for i := 0; i < 8; i++ {
+				if tile.Pix[j*8+i] == opaqueColor {
+					glyph.Pix[(j+o*8)*8+i] = 1
+				}
+			}
+		}
+	}
+
+	return glyph, nil
 }
