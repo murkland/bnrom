53d1976f52d2657acca6d3fb97153106311b5fd3
bigfarts <bigfarts@punymail.com>
2022-03-06 12:38:43 -0800

    dump fonts from US/EU

--- fonts/mod.go
+++ fonts/mod.go
@@ -6,6 +6,8 @@ type ROMInfo struct {
 
 func FindROMInfo(romID string) *ROMInfo {
 	switch romID {
+	case "BR6E", "BR6P", "BR5E", "BR5P":
+		return &ROMInfo{0x0001D854}
 	case "BR6J", "BR5J":
 		return &ROMInfo{0x0001DC78}
 	}
